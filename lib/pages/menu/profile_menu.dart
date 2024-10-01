9f9681582f1f8c517e017344ac8232bd9c9815b6
rico <narayanarico25@gmail.com>
2024-10-01 22:43:20 +0700

    feat[NC]profile UI

--- lib/pages/menu/profile_menu.dart
+++ lib/pages/menu/profile_menu.dart
@@ -1,13 +1,77 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:ricomusic/widget/mycolor.dart'; 
 class ProfileMenu extends StatelessWidget {
   const ProfileMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Ini Profile"),
+      body: Column(
+        children: [
+         
+          Stack(
+            clipBehavior: Clip.none,
+            alignment: Alignment.center,
+            children: [
+              
+              Container(
+                height: 250, 
+                decoration: BoxDecoration(
+                  color: darkBlue,
+                  borderRadius: BorderRadius.vertical(
+                    bottom: Radius.circular(30),
+                  ),
+                ),
+              ),
+
+              Positioned(
+                bottom: -50, 
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('images/ariana.png'),
+                ),
+              ),
+            ],
+          ),
+
+          // Text Section
+          SizedBox(height: 60), 
+          Text(
+            "Hi, Taylor Swift",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: darkBlue, 
+            ),
+          ),
+
+          Spacer(), 
+
+          // Exit Button
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: ElevatedButton(
+              onPressed: () {
+                // Exit functionality
+                    Get.toNamed('/'); 
+                  
+              },
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                backgroundColor: darkBlue, 
+              ),
+              child: Text(
+                'Exit',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
